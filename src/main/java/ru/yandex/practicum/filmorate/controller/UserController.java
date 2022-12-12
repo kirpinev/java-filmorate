4bcd77d30c90d4b6df3d0a257e587e22d6df6e5d
Igor Kirpinev <kirpinev@yahoo.com>
2022-12-12 20:24:48 +0300

    fix: Правки после ревью

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -2,10 +2,8 @@ package ru.yandex.practicum.filmorate.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.service.UserService;
-import ru.yandex.practicum.filmorate.validation.UserValidator;
 
 import javax.validation.Valid;
 import java.util.ArrayList;
@@ -17,7 +15,6 @@ import java.util.List;
 @RequestMapping("/users")
 public class UserController {
 
-    private static final String NOT_FOUND_MESSAGE = "пользователя с id %s нет";
     private final UserService userService;
 
     public UserController(UserService userService) {
@@ -27,8 +24,6 @@ public class UserController {
     @PostMapping
     public User createUser(@Valid @RequestBody User user) {
         log.debug("Создается пользователь: {}", user);
-
-        setUserName(user);
 
         return userService.createUser(user);
     }
@@ -40,19 +35,11 @@ public class UserController {
 
     @GetMapping("/{id}")
     public User getUserById(@PathVariable("id") Integer id) {
-        User user = userService.getUserById(id);
-
-        checkUserIsNotNull(user, id);
-
         return userService.getUserById(id);
     }
 
     @GetMapping("/{id}/friends")
     public List<User> getUserFriends(@PathVariable("id") Integer id) {
-        User user = userService.getUserById(id);
-
-        checkUserIsNotNull(user, id);
-
         return userService.getUserFriends(id);
     }
 
@@ -61,19 +48,12 @@ public class UserController {
         User user1 = userService.getUserById(id);
         User user2 = userService.getUserById(otherId);
 
-        checkUserIsNotNull(user1, id);
-        checkUserIsNotNull(user2, otherId);
-
         return userService.getCommonFriends(user1, user2);
     }
 
     @PutMapping
     public User updateUser(@Valid @RequestBody User user) {
         log.debug("Обновляется пользователь: {}", user);
-
-        checkUserIsNotNull(user, user.getId());
-
-        setUserName(user);
 
         return userService.updateUser(user);
     }
@@ -82,9 +62,6 @@ public class UserController {
     public void addFriendToUser(@PathVariable("id") Integer id, @PathVariable("friendId") Integer friendId) {
         User user = userService.getUserById(id);
         User friend = userService.getUserById(friendId);
-
-        checkUserIsNotNull(user, id);
-        checkUserIsNotNull(friend, friendId);
 
         userService.addFriendToUser(user, friend);
         userService.addFriendToUser(friend, user);
@@ -95,22 +72,7 @@ public class UserController {
         User user = userService.getUserById(id);
         User friend = userService.getUserById(friendId);
 
-        checkUserIsNotNull(user, id);
-        checkUserIsNotNull(friend, friendId);
-
         userService.deleteFriendFromUser(user, friend);
         userService.deleteFriendFromUser(friend, user);
-    }
-
-    private void setUserName(User user) {
-        if (!UserValidator.isUserNameValid(user.getName())) {
-            user.setName(user.getLogin());
-        }
-    }
-
-    private void checkUserIsNotNull(User user, Integer id) {
-        if (UserValidator.isUserNotFound(userService.getUsers(), user)) {
-            throw new NotFoundException(String.format(NOT_FOUND_MESSAGE, id));
-        }
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -1,8 +1,10 @@
 package ru.yandex.practicum.filmorate.service;
 
 import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.user.UserStorage;
+import ru.yandex.practicum.filmorate.validation.UserValidator;
 
 import java.util.List;
 import java.util.Map;
@@ -12,6 +14,7 @@ import java.util.stream.Collectors;
 @Service
 public class UserService {
 
+    private static final String NOT_FOUND_MESSAGE = "пользователя с id %s нет";
     private final UserStorage userStorage;
 
     public UserService(UserStorage inMemoryUserStorage) {
@@ -19,6 +22,8 @@ public class UserService {
     }
 
     public User createUser(User user) {
+        setUserName(user);
+
         return userStorage.create(user);
     }
 
@@ -27,11 +32,18 @@ public class UserService {
     }
 
     public User getUserById(Integer id) {
-        return userStorage.getById(id);
+        User user = userStorage.getById(id);
+
+        checkUserIsNotNull(user, id);
+
+        return user;
     }
 
     public List<User> getUserFriends(Integer id) {
-        User user = getUserById(id);
+        User user = userStorage.getById(id);
+
+        checkUserIsNotNull(user, id);
+
         Map<Integer, User> users = getUsers();
 
         return user
@@ -42,6 +54,9 @@ public class UserService {
     }
 
     public List<User> getCommonFriends(User user1, User user2) {
+        checkUserIsNotNull(user1, user1.getId());
+        checkUserIsNotNull(user2, user2.getId());
+
         Map<Integer, User> users = getUsers();
         Set<Integer> user1Friends = user1.getFriends();
         Set<Integer> user2Friends = user2.getFriends();
@@ -54,15 +69,31 @@ public class UserService {
     }
 
     public User updateUser(User user) {
+        checkUserIsNotNull(user, user.getId());
+        setUserName(user);
         return userStorage.update(user);
     }
 
     public void addFriendToUser(User user, User friend) {
+        checkUserIsNotNull(user, user.getId());
         user.getFriends().add(friend.getId());
         userStorage.update(user);
     }
 
     public void deleteFriendFromUser(User user, User friend) {
+        checkUserIsNotNull(user, user.getId());
         user.getFriends().remove(friend.getId());
+    }
+
+    private void checkUserIsNotNull(User user, Integer id) {
+        if (UserValidator.isUserNotFound(getUsers(), user)) {
+            throw new NotFoundException(String.format(NOT_FOUND_MESSAGE, id));
+        }
+    }
+
+    private void setUserName(User user) {
+        if (!UserValidator.isUserNameValid(user.getName())) {
+            user.setName(user.getLogin());
+        }
     }
 }
