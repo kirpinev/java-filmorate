f659b60cddcda785be3ffaf0a4dcd1b023d032de
MaricTheirin <ghostmeee@yandex.ru>
2023-01-26 19:34:54 +0300

    feat: Реализовать сохранение/получение по id/проверку существования для review

--- src/main/java/ru/yandex/practicum/filmorate/storage/filmReview/ReviewDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/filmReview/ReviewDbStorage.java
@@ -1,18 +1,60 @@
 package ru.yandex.practicum.filmorate.storage.filmReview;
 
-import org.springframework.beans.factory.annotation.Qualifier;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.model.Review;
-
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
-@Qualifier("ReviewDbStorage")
-@Component
+@Slf4j
+@Component("ReviewDbStorage")
 public class ReviewDbStorage implements ReviewStorage {
 
+    private final JdbcTemplate template;
+    private static final String GET_REVIEW_BASE_QUERY =
+            "SELECT review_id, film_id, user_id, useful, is_positive, content FROM reviews ";
+    private static final String GET_REVIEW_BY_ID_QUERY =
+            GET_REVIEW_BASE_QUERY + " WHERE review_id = ?";
+    private static final String SAVE_REVIEW_QUERY =
+            "INSERT INTO reviews (film_id, user_id, useful, is_positive, content) VALUES (?, ?, ?, ?, ?)";
+    private static final String CHECK_REVIEW_EXISTS_BY_ID_QUERY =
+            "SELECT EXISTS(SELECT review_id FROM reviews WHERE review_id = ?) isExists";
+
+    @Autowired
+    public ReviewDbStorage(JdbcTemplate template) {
+        this.template = template;
+    }
+
     @Override
-    public Review add(Review filmReview) {
-        return null;
+    public Review add(Review review) {
+        KeyHolder holder = new GeneratedKeyHolder();
+        int savedReviewId;
+
+        template.update(conn -> {
+            PreparedStatement smt = conn.prepareStatement(SAVE_REVIEW_QUERY, new String[] {"review_id"});
+            smt.setInt(1, review.getFilmId());
+            smt.setInt(2, review.getUserId());
+            smt.setInt(3, review.getUseful());
+            smt.setBoolean(4, review.getIsPositive());
+            smt.setString(5, review.getContent());
+            return smt;
+        }, holder);
+
+        try {
+            savedReviewId = holder.getKey().intValue();
+        } catch (NullPointerException e) {
+            log.warn("Ошибка при получении id записываемого в БД отзыва {}: {}", review, e.getMessage());
+            throw e;
+        }
+        log.debug("Отзыв записан с id = {}", savedReviewId);
+
+        return getById(savedReviewId);
     }
 
     @Override
@@ -27,7 +69,7 @@ public class ReviewDbStorage implements ReviewStorage {
 
     @Override
     public Review getById(Integer id) {
-        return null;
+        return template.queryForObject(GET_REVIEW_BY_ID_QUERY, this::mapRowToReview, id);
     }
 
     @Override
@@ -42,6 +84,20 @@ public class ReviewDbStorage implements ReviewStorage {
 
     @Override
     public boolean isExists(Integer id) {
-        return false;
+        return Boolean.TRUE.equals(template.queryForObject(
+                CHECK_REVIEW_EXISTS_BY_ID_QUERY, (rs, rowNum) -> rs.getBoolean("isExists"), id
+        ));
     }
+
+    private Review mapRowToReview(ResultSet rs, int rowNum) throws SQLException {
+        return Review.builder()
+                .id(rs.getInt("review_id"))
+                .filmId(rs.getInt("film_id"))
+                .userId(rs.getInt("user_id"))
+                .useful(rs.getInt("useful"))
+                .isPositive(rs.getBoolean("is_positive"))
+                .content(rs.getString("content"))
+                .build();
+    }
+
 }
