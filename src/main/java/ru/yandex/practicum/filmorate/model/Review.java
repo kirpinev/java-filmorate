2b64a260f4d4266874ecfc4ced08810a74f47595
MaricTheirin <ghostmeee@yandex.ru>
2023-02-03 12:33:26 +0300

    chore: Добавить AllArgsConstructor, NoArgsConstructor

--- src/main/java/ru/yandex/practicum/filmorate/model/Review.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Review.java
@@ -1,12 +1,16 @@
 package ru.yandex.practicum.filmorate.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Review {
 
     Integer reviewId;
